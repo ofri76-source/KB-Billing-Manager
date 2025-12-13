bd82d60839de4d94bdbd221d87c77a6a51115ee5
ofri76-source <ofri76@gmail.com>
2025-12-13 08:59:59 +0200

    Avoid duplicate combined license type declarations

--- m365-license-manager.php
+++ m365-license-manager.php
@@ -18,6 +18,7 @@ define('M365_LM_PLUGIN_URL', plugin_dir_url(__FILE__));
 // טעינת קבצים נדרשים
 require_once M365_LM_PLUGIN_DIR . 'includes/class-database.php';
 require_once M365_LM_PLUGIN_DIR . 'includes/class-api-connector.php';
+require_once M365_LM_PLUGIN_DIR . 'includes/class-partner-connector.php';
 require_once M365_LM_PLUGIN_DIR . 'includes/class-shortcodes.php';
 require_once M365_LM_PLUGIN_DIR . 'includes/class-admin.php';
 
@@ -28,9 +29,17 @@ add_action('admin_post_nopriv_kbbm_download_script', 'kbbm_download_script_handl
 register_activation_hook(__FILE__, 'kb_billing_manager_activate');
 register_deactivation_hook(__FILE__, 'm365_lm_deactivate');
 
+// תיקון סכימה גם לאחר שדרוגים
+add_action('admin_init', 'kb_billing_manager_maybe_install');
+
 function kb_billing_manager_activate() {
     M365_LM_Database::create_tables();
     flush_rewrite_rules();
+}
+
+function kb_billing_manager_maybe_install() {
+    // מריץ את יצירת/תיקון הטבלאות גם לאחר שדרוגים כדי להוסיף עמודות חסרות
+    M365_LM_Database::create_tables();
 }
 
 // שמירה על תאימות לאחור
